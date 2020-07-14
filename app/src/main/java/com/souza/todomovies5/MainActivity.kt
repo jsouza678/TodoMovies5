8c24fcb81158ab92ad45e43ecacbfa3551e4fb86
João Victor Souza <jsouza678@icloud.com>
2020-07-14 19:26:42 -0300

    Initial commit

---
+++ app/src/main/java/com/souza/todomovies5/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.souza.todomovies5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
