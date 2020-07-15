526748a27326c781ccbe956932489d6871cda235
João Victor Souza <jsouza678@icloud.com>
2020-07-15 00:39:51 -0300

    Setup layout for details activity; Fonts, Colors, Dimens and Strings added to project; Extracted TextView Styles to Styles file on res/values; Material Design and RecyclerView added to gradle; Renamed MainActivity to DetailsActivity; Icons to "favorite/like" added; Collapsing Toolbar working with Motion Layout -> collapsing a ImageView in it;

--- app/src/main/java/com/souza/todomovies5/presentation/DetailsActivity.kt
+++ app/src/main/java/com/souza/todomovies5/presentation/DetailsActivity.kt
@@ -1,13 +1,13 @@
 package com.souza.todomovies5.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.souza.todomovies5.R
 
-class MainActivity : AppCompatActivity() {
+class DetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_details)
     }
 }
